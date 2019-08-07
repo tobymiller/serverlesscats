26c16a8721a4f0fb79335696a5617e2092206a78
Toby Miller <tobycmiller@gmail.com>
2019-08-07 15:09:40 +0100

    context shift

--- src/main/scala/uk/me/tobymiller/serverlesscats/Handler.scala
+++ src/main/scala/uk/me/tobymiller/serverlesscats/Handler.scala
@@ -1,9 +1,9 @@
 package uk.me.tobymiller.serverlesscats
 
-import cats.effect.IO
+import cats.effect.{ContextShift, IO}
 import io.circe.Json
 import cats.data.EitherT
 
 trait Handler {
-  def handle(input: IO[Json]): EitherT[IO, Throwable, Json]
+  def handle(input: IO[Json])(implicit cs: ContextShift[IO]): EitherT[IO, Throwable, Json]
 }
--- src/main/scala/uk/me/tobymiller/serverlesscats/ServerlessHost.scala
+++ src/main/scala/uk/me/tobymiller/serverlesscats/ServerlessHost.scala
@@ -5,22 +5,30 @@ import cats.data._
 import cats.implicits._
 import java.io.OutputStream
 import java.io.InputStream
+
 import com.amazonaws.services.lambda.runtime.Context
-import io.circe._, io.circe.parser._
+import io.circe._
+import io.circe.parser._
 import java.io.BufferedReader
 import java.io.InputStreamReader
+
 import scala.language.postfixOps
 import cats.effect._
 import cats.effect.implicits._
 import java.io.IOException
 import java.io.BufferedWriter
 import java.io.OutputStreamWriter
+
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.nio.charset.StandardCharsets
 
+import scala.concurrent.ExecutionContext
+
 class ServerlessHost(handler: Handler) extends RequestStreamHandler {
+  implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+
   private def catchAll[E](in: => E) = EitherT.fromEither[IO](Either.catchNonFatal[E](in))
   
   def handleRequest(is: InputStream, os: OutputStream, context: Context): Unit = {
